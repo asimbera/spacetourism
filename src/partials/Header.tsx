09712a6920321bd8cc263bf98d3ae6dca9107335
Asim Bera <asimbera@outlook.in>
2021-11-02 19:51:54 +0530

    Add mobile sidebar Fixes #6

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,6 +1,8 @@
 import { ClassNames } from '@emotion/react';
 import styled from '@emotion/styled';
 import media from 'css-in-js-media';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { getAssetUrl } from '../utils';
 
@@ -9,7 +11,7 @@ const Header = styled.header`
   top: 0;
   left: 0;
   width: 100%;
-  z-index: 1000;
+  z-index: 100;
   padding: 24px;
   display: flex;
   align-items: center;
@@ -83,37 +85,174 @@ const DesktopOnly = styled.b`
   }
 `;
 
-export default () => (
-  <Header>
-    <Logo src={getAssetUrl('/images/shared/logo.svg')} alt='Logo' />
-    <Hamburger>
-      <img
-        src={getAssetUrl('/images/shared/icon-hamburger.svg')}
-        alt='Hamburger'
-      />
-    </Hamburger>
-    <ClassNames>
-      {({ css, cx }) => {
-        const activeStyle = css`
-          border-bottom: 3px solid var(--clr-white);
-        `;
-        return (
-          <Navbar>
-            <NavItem to='/home' activeClassName={activeStyle}>
-              <DesktopOnly>00</DesktopOnly> Home
-            </NavItem>
-            <NavItem to='/dest' activeClassName={activeStyle}>
-              <DesktopOnly>01</DesktopOnly> Destination
-            </NavItem>
-            <NavItem to='/crew' activeClassName={activeStyle}>
-              <DesktopOnly>02</DesktopOnly> Crew
-            </NavItem>
-            <NavItem to='/tech' activeClassName={activeStyle}>
-              <DesktopOnly>03</DesktopOnly> Technology
-            </NavItem>
-          </Navbar>
-        );
-      }}
-    </ClassNames>
-  </Header>
-);
+const Sidebar = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 254px;
+  height: 100vh;
+  z-index: 101;
+  padding: 24px;
+  padding-right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: hsla(0, 0%, 100%, 0.04);
+  backdrop-filter: blur(32px);
+
+  ${media('>=tablet')} {
+    display: none;
+  }
+`;
+
+const HamburgerClose = styled.button`
+  background-color: transparent;
+  border: 0;
+  align-self: flex-end;
+  padding-right: 24px;
+`;
+
+const SidebarMenu = styled.ul`
+  margin-top: 64px;
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+
+  * + * {
+    margin-top: 32px;
+  }
+`;
+
+const SidebarMenuItem = styled.li`
+  font-family: 'Barlow Condensed', sans-serif;
+  font-size: 16px;
+  letter-spacing: 2.7px;
+  color: var(--clr-white);
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const SidebarMenuItemNumber = styled.b`
+  &::after {
+    content: '       ';
+    width: 14px;
+  }
+`;
+
+export default () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const openSidebar = () => setSidebarOpen(true);
+  const closeSidebar = () => setSidebarOpen(false);
+
+  return (
+    <React.Fragment>
+      <Header>
+        <Logo src={getAssetUrl('/images/shared/logo.svg')} alt='Logo' />
+        <Hamburger onClick={openSidebar}>
+          <img
+            src={getAssetUrl('/images/shared/icon-hamburger.svg')}
+            alt='Hamburger'
+          />
+        </Hamburger>
+        <ClassNames>
+          {({ css, cx }) => {
+            const activeStyle = css`
+              border-bottom: 3px solid var(--clr-white);
+            `;
+            return (
+              <Navbar>
+                <NavItem to='/home' activeClassName={activeStyle}>
+                  <DesktopOnly>00</DesktopOnly> Home
+                </NavItem>
+                <NavItem to='/dest' activeClassName={activeStyle}>
+                  <DesktopOnly>01</DesktopOnly> Destination
+                </NavItem>
+                <NavItem to='/crew' activeClassName={activeStyle}>
+                  <DesktopOnly>02</DesktopOnly> Crew
+                </NavItem>
+                <NavItem to='/tech' activeClassName={activeStyle}>
+                  <DesktopOnly>03</DesktopOnly> Technology
+                </NavItem>
+              </Navbar>
+            );
+          }}
+        </ClassNames>
+      </Header>
+      <AnimatePresence exitBeforeEnter>
+        {sidebarOpen && (
+          <Sidebar
+            initial={{ x: '100%' }}
+            animate={{ x: 0 }}
+            exit={{ x: '100%' }}
+            transition={{ duration: 0.5 }}
+          >
+            <HamburgerClose onClick={closeSidebar}>
+              <img
+                src={getAssetUrl('/images/shared/icon-close.svg')}
+                alt='Close'
+              />
+            </HamburgerClose>
+            <ClassNames>
+              {({ css, cx }) => {
+                const active = css`
+                  border-right: 3px solid #ffffff;
+                `;
+                const inactive = css`
+                  width: 100%;
+                  color: #ffffff;
+                `;
+
+                return (
+                  <SidebarMenu>
+                    <SidebarMenuItem>
+                      <NavLink
+                        to='/home'
+                        className={inactive}
+                        activeClassName={active}
+                      >
+                        <SidebarMenuItemNumber>00</SidebarMenuItemNumber>Home
+                      </NavLink>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <NavLink
+                        to='/dest'
+                        className={inactive}
+                        activeClassName={active}
+                      >
+                        <SidebarMenuItemNumber>01</SidebarMenuItemNumber>
+                        Destination
+                      </NavLink>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <NavLink
+                        to='/crew'
+                        className={inactive}
+                        activeClassName={active}
+                      >
+                        <SidebarMenuItemNumber>02</SidebarMenuItemNumber>Crew
+                      </NavLink>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <NavLink
+                        to='/tech'
+                        className={inactive}
+                        activeClassName={active}
+                      >
+                        <SidebarMenuItemNumber>03</SidebarMenuItemNumber>
+                        Technology
+                      </NavLink>
+                    </SidebarMenuItem>
+                  </SidebarMenu>
+                );
+              }}
+            </ClassNames>
+          </Sidebar>
+        )}
+      </AnimatePresence>
+    </React.Fragment>
+  );
+};
